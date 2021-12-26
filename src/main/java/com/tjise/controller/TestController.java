dbe79ab5048185298022f7eb1ee51b50f436100e
lzjcpx <240582597@qq.com>
2021-12-26 23:06:48 +0800

    init programe

---
+++ src/main/java/com/tjise/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.tjise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @auther shkstart
+ * @create 2021-12-26-22:19
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+}
